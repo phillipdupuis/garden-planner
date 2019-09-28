87e36b4b55eafa9c01913e70dd0edaa705e6a452
Phil <phillip_dupuis@alumni.brown.edu>
2019-09-28 14:55:04 -0400

    .

---
+++ garden_planner/frontend/build/precache-manifest.1422adaa2465909658836836166717a4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005c03bce0f3ad6bf4d28898c6427d51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a6e539d30b40da1b4a7",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3139766fb451f1507c75",
+    "url": "/static/css/main.efec41a1.chunk.css"
+  },
+  {
+    "revision": "4a6e539d30b40da1b4a7",
+    "url": "/static/js/2.dca5de83.chunk.js"
+  },
+  {
+    "revision": "3139766fb451f1507c75",
+    "url": "/static/js/main.e5534056.chunk.js"
+  },
+  {
+    "revision": "65a81b1ca9a7620b8ceb",
+    "url": "/static/js/runtime-main.1dc4db23.js"
+  },
+  {
+    "revision": "ee995698260962b78b726865dc110182",
+    "url": "/static/media/beet.ee995698.svg"
+  },
+  {
+    "revision": "2266c86028cd987d2ef475f15eec05fc",
+    "url": "/static/media/carrot.2266c860.svg"
+  },
+  {
+    "revision": "872640e12bf41dfd4ea563a8b6bbb2bd",
+    "url": "/static/media/cat.872640e1.svg"
+  },
+  {
+    "revision": "e1631c506237067f64e40acb3fb620a7",
+    "url": "/static/media/garlic.e1631c50.svg"
+  },
+  {
+    "revision": "f6dc4275919bea650d1d62cdbecf3e1a",
+    "url": "/static/media/leek.f6dc4275.svg"
+  },
+  {
+    "revision": "bf643227195f45ddac70833481c8daad",
+    "url": "/static/media/lettuce.bf643227.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "6e5e1a95289b6ea0891e465b45989e8d",
+    "url": "/static/media/onion.6e5e1a95.svg"
+  },
+  {
+    "revision": "ec87128428934824193e96208df34f6d",
+    "url": "/static/media/peas.ec871284.svg"
+  },
+  {
+    "revision": "43c53b88e11c8d34c2ff6dcbea75803c",
+    "url": "/static/media/radish.43c53b88.svg"
+  },
+  {
+    "revision": "d2107f442e6ceb730b8283db998c5092",
+    "url": "/static/media/redo.d2107f44.svg"
+  },
+  {
+    "revision": "5317055ccb7b04d2e440b70d7c4317f0",
+    "url": "/static/media/shovel.5317055c.svg"
+  },
+  {
+    "revision": "fb59bf7b8b1032f21dc83658d1b8a5dd",
+    "url": "/static/media/spinach.fb59bf7b.svg"
+  },
+  {
+    "revision": "b7931bae80e691c7531de66c447bb572",
+    "url": "/static/media/strawberry.b7931bae.svg"
+  },
+  {
+    "revision": "4826181a9b05a17c486fe9c57685211f",
+    "url": "/static/media/tomato.4826181a.svg"
+  },
+  {
+    "revision": "42f85826578323bfe42368c63c93b3c0",
+    "url": "/static/media/undo.42f85826.svg"
+  }
+]);
