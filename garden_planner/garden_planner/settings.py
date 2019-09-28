f5daf70cd2052c90b05157d079f9cafb23c9a399
Phil <phillip_dupuis@alumni.brown.edu>
2019-09-28 14:21:44 -0400

    added an entry point to the react app through django

--- garden_planner/garden_planner/settings.py
+++ garden_planner/garden_planner/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static'),
+]
--- garden_planner/garden_planner/urls.py
+++ garden_planner/garden_planner/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('backend/', include('backend.urls')),
+    re_path(r'^', views.FrontendAppView.as_view()),
 ]
